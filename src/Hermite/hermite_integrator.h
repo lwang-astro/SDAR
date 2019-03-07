90320b0dedc8105a1b40e8397b2d5ac0d9a048d8
Long Wang <longwang.astro@gmail.com>
2019-03-07 16:28:43 +0100

    Add predicted group break criterion

--- src/Hermite/hermite_integrator.h
+++ src/Hermite/hermite_integrator.h
@@ -1261,28 +1261,61 @@ namespace H4{
                 
                 auto& bin_root = groupk.info.binarytree.getLastMember();
 
-                // check distance criterion and outcome (ecca>0) or income (ecca<0)
-                if (bin_root.r > manager->r_break_crit) {
-                    // close orbit
-                    if (bin_root.semi>0.0 && bin_root.ecca>0.0) {
+                // check binary 
+                if (bin_root.semi>0.0 && bin_root.ecca>0.0) {
+                    // check distance criterion and outcome (ecca>0) or income (ecca<0)
+                    if (bin_root.r > manager->r_break_crit) {
 #ifdef ADJUST_GROUP_DEBUG
                         std::cerr<<"Break group: binary escape, i_group: "<<k<<" N_member: "<<n_member<<" ecca: "<<bin_root.ecca<<" separation : "<<bin_root.r<<" r_crit: "<<manager->r_break_crit<<std::endl;
 #endif
                         _break_group_index_with_offset[_n_break++] = k + index_offset_group_;
                         continue;
                     }
-                    else { // hyperbolic case, ecca is not correctly calculated
+
+                    // check wide binary case for next step
+                    if (bin_root.semi*(1.0+bin_root.ecc)>manager->r_break_crit) {
                         Float dr2, drdv;
                         groupk.info.getDrDv(dr2, drdv, *bin_root.getLeftMember(), *bin_root.getRightMember());
-                        if (drdv>0.0) {
+                        ASSERT(drdv>=0.0);
+
+                        // check next step the radial distance
+                        Float rp = drdv/bin_root.r*groups[k].particles.cm.dt + bin_root.r;
+                        if (rp >manager->r_break_crit) {
 #ifdef ADJUST_GROUP_DEBUG
-                            std::cerr<<"Break group: hyperbolic escape, i_group: "<<k<<" N_member: "<<n_member<<" drdv: "<<drdv<<" separation : "<<bin_root.r<<" r_crit: "<<manager->r_break_crit<<std::endl;
+                            std::cerr<<"Break group: binary will escape, i_group: "<<k<<" N_member: "<<n_member<<" ecca: "<<bin_root.ecca<<" separation : "<<bin_root.r<<" r_pred: "<<rp<<" drdv: "<<drdv<<" dt: "<<groups[k].particles.cm.dt<<" r_crit: "<<manager->r_break_crit<<std::endl;
 #endif
                             _break_group_index_with_offset[_n_break++] = k + index_offset_group_;
                             continue;
                         }
                     }
                 }
+
+                // check hyperbolic case
+                if (bin_root.semi<0.0) {
+                    // hyperbolic case, ecca is not correctly calculated
+                    Float dr2, drdv;
+                    groupk.info.getDrDv(dr2, drdv, *bin_root.getLeftMember(), *bin_root.getRightMember());
+                    if (drdv>0.0) {
+                        // check distance criterion
+                        if (bin_root.r > manager->r_break_crit) {
+#ifdef ADJUST_GROUP_DEBUG
+                            std::cerr<<"Break group: hyperbolic escape, i_group: "<<k<<" N_member: "<<n_member<<" drdv: "<<drdv<<" separation : "<<bin_root.r<<" r_crit: "<<manager->r_break_crit<<std::endl;
+#endif
+                            _break_group_index_with_offset[_n_break++] = k + index_offset_group_;
+                            continue;
+                        }
+                        // check for next step
+                        Float rp = drdv/bin_root.r*groups[k].particles.cm.dt + bin_root.r;
+                        if (rp > manager->r_break_crit) {
+#ifdef ADJUST_GROUP_DEBUG
+                            std::cerr<<"Break group: hyperbolic will escape, i_group: "<<k<<" N_member: "<<n_member<<" drdv: "<<drdv<<" separation : "<<bin_root.r<<" r_pred: "<<rp<<" drdv: "<<drdv<<" dt: "<<groups[k].particles.cm.dt<<" r_crit: "<<manager->r_break_crit<<std::endl;
+#endif
+                            _break_group_index_with_offset[_n_break++] = k + index_offset_group_;
+                            continue;
+                        }
+                    }
+                }
+
                 ASSERT(bin_root.r<2.0*manager->r_break_crit);
    
                 // check strong perturbed binary case
